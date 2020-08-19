807af652cafcae90ec20a146c69060269d487db4
Saku Pietilä <spietila@QFINM291.local>
2020-08-19 17:41:09 +0300

    Some tinkering

--- apicall.py
+++ apicall.py
@@ -1,2 +1,32 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+import urllib.request
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+ioc_url = "https://urlhaus.abuse.ch/downloads/csv_recent/"
+threat_key = ""
+headers = ""
+
+
+print('Starting data retrieval!\n')
+
+try:
+    r = urllib.request.urlopen(ioc_url)
+except urllib.request.HTTPError as e:
+    if e.code == 404:
+        print('404')
+    else:
+        print('jotain')
+except urllib.request.HTTPError as e:
+    print('Cant connect')
+else:
+    # 200
+    print('Data retrieval complete!\n')
+    body = r.read()
+
+print('Done!')
+
+from IPython import embed; embed()
 
