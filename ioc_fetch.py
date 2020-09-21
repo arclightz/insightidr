ba74f5a5d8ab294e21033a20b419e4d8392208f6
Saku Pietilä <spietila@iki.fi>
2020-09-21 08:59:18 +0300

    Delete ioc_fetch.py

--- ioc_fetch.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-url="https://urlhaus.abuse.ch/downloads/csv"
-c2="https://raw.githubusercontent.com/ring0x0/emotet-configs/master/master_list_unique.txt"
-bulk="bulk.txt"
-malware="emotet"
-
-
